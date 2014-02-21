295fe17be3027b2ebc7a3ee2cf7975401970e1dc
Alexis Métaireau <alexis@notmyidea.org>
2014-02-21 12:49:41 +0100

    Start unit testing the POST /call-url URL

--- loop/index.js
+++ loop/index.js
@@ -2,9 +2,16 @@
 
 var express = require('express');
 var app = express();
+app.use(express.json());
 
-app.get('/', function(req, res){
-  res.send('Hello World');
+app.post('/call-url', function(req, res){
+  req.accepts('json');
+  if (!('simple_push_url' in req.body)){
+    return res.json(400, {error: 'simple_push_url is required'});
+  }
+  console.log(req.body);
 });
 
 app.listen(5000);
+
+module.exports = app;
--- test/functional_test.js
+++ test/functional_test.js
@@ -1,17 +1,31 @@
 /* global it, describe */
 
 var expect = require("chai").expect;
+var request = require("supertest");
+
+var app = require("../loop");
 
 describe("HTTP API exposed by the server", function() {
   "use strict";
+  var req;
+
+  beforeEach(function(){
+    req = request(app)
+            .post('/call-url')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/);
+  });
 
   describe("POST /call-url", function() {
-    it.skip("should require simple push url", function() {
-
+    it("should require simple push url", function() {
+      req.expect(400).end(function(err, res){
+        if (err) throw err;
+        expect(res.body.error).eql('simple_push_url is required');
+      });
     });
 
     it.skip("should validate the simple push url", function() {
-
+      
     });
 
     it.skip("should attach a session to the user agent", function() {
