4e9a6b36ad8b59d94ed558fe28cb9829b5178ded
Nicolas Perriault <nperriault@gmail.com>
2014-02-22 11:51:12 +0100

    extracted request data validation to its own helper.

--- loop/index.js
+++ loop/index.js
@@ -6,21 +6,32 @@ var app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
+function validateCallUrl(reqDataObj) {
+  if (typeof reqDataObj !== 'object')
+    throw new Error('missing request data');
+
+  if (!reqDataObj.hasOwnProperty('simple_push_url'))
+    throw new Error('simple_push_url is required');
+
+  if (reqDataObj.simple_push_url.indexOf('http') !== 0)
+    throw new Error('simple_push_url should be a valid url');
+
+  return reqDataObj;
+}
+
 app.post('/call-url', function(req, res) {
+  var validated;
+
   if (req.headers['content-type'] !== 'application/json')
     return res.json(406, ['application/json']);
 
-  // XXX this is never reached in case of empty request body sent, investigate
-  if (typeof req.body !== 'object')
-    return res.json(400, {error: 'missing request data'});
+  try {
+    validated = validateCallUrl(req.body);
+  } catch (err) {
+    return res.json(400, {error: err.message});
+  }
 
-  if (!req.body.hasOwnProperty('simple_push_url'))
-    return res.json(400, {error: 'simple_push_url is required'});
-
-  if (req.body.simple_push_url.indexOf('http') !== 0)
-    return res.json(400, {error: 'simple_push_url should be a valid url'});
-
-  return res.json(200, {ok: true}); // XXX to be continued
+  return res.json(200, {validated: validated}); // XXX to be continued
 });
 
 app.listen(5000);
