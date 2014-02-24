a8a3c941fd8cc7a1439fd8a3133c1a7579c63f7c
Nicolas Perriault <nperriault@gmail.com>
2014-02-24 14:01:20 +0100

    Refactored storage adapter tests to reuse the same suite.

--- test/storage/storage_test.js
+++ test/storage/storage_test.js
@@ -45,45 +45,37 @@ describe("Storage", function() {
   });
 });
 
-describe("MongoAdapter", function() {
+describe("Adapters", function() {
   "use strict";
 
-  describe("#constructor", function() {
-    it("should require a DSN", function() {
-      expect(function() {
-        new MongoAdapter();
-      }).Throw(Error, /A DSN is required/);
+  describe("MongoAdapter", function() {
+    describe("#constructor", function() {
+      it("should require a DSN", function() {
+        expect(function() {
+          new MongoAdapter();
+        }).Throw(Error, /A DSN is required/);
+      });
     });
   });
 
-  describe("constructed", function() {
-    var adapter;
+  // both adapters implements the same interface, so using the very same suite
+  function testAdapter(name, createFn) {
+    describe(name, function() {
+      var adapter;
 
-    beforeEach(function() {
-      adapter = new MongoAdapter('mongodb://127.0.0.1:27017/loop_test');
-    });
-
-    afterEach(function(done) {
-      adapter.drop(function(err) {
-        done(err);
+      beforeEach(function() {
+        adapter = createFn();
       });
-    });
 
-    describe("#addOne", function() {
-      it("should add a record to a given collection", function(done) {
-        adapter.addOne("test_coll", {a: 1, b: 2}, function(err, record) {
-          expect(err).to.be.a("null");
-          expect(record).to.be.a("object");
-          expect(record.a).eql(1);
-          done();
+      afterEach(function(done) {
+        adapter.drop(function(err) {
+          done(err);
         });
       });
-    });
 
-    describe("#getOne", function() {
-      it("should retrieve a record out of a query object", function(done) {
-        adapter.addOne("test_coll", {a: 1, b: 2}, function(err) {
-          adapter.getOne("test_coll", {a: 1}, function(err, record) {
+      describe("#addOne", function() {
+        it("should add a record to a given collection", function(done) {
+          adapter.addOne("test_coll", {a: 1, b: 2}, function(err, record) {
             expect(err).to.be.a("null");
             expect(record).to.be.a("object");
             expect(record.a).eql(1);
@@ -92,91 +84,50 @@ describe("MongoAdapter", function() {
         });
       });
 
-      it("should give an error on no record found", function(done) {
-        adapter.getOne("test_coll", {x: 42}, function(err, record) {
-          expect(err).to.be.a.instanceOf(Error);
-          expect(err.message).to.match(/No record found matching query/);
-          done();
-        });
-      });
-    });
-
-    describe("#drop", function() {
-      it("should drop the database", function(done) {
-        adapter.addOne("bar", {a: 1}, function() {
-          adapter.drop(function(err) {
-            expect(err).to.be.a("null");
+      describe("#getOne", function() {
+        it("should retrieve a record out of a query object", function(done) {
+          adapter.addOne("test_coll", {a: 1, b: 2}, function(err) {
             adapter.getOne("test_coll", {a: 1}, function(err, record) {
-              expect(err).to.be.a("object");
+              expect(err).to.be.a("null");
+              expect(record).to.be.a("object");
+              expect(record.a).eql(1);
               done();
             });
           });
         });
-      });
-    });
-  });
-});
 
-describe("MemoryAdapter", function() {
-  "use strict";
-
-  describe("constructed", function() {
-    var adapter;
-
-    beforeEach(function() {
-      adapter = new MemoryAdapter();
-    });
-
-    afterEach(function(done) {
-      adapter.drop(function(err) {
-        done(err);
-      });
-    });
-
-    describe("#addOne", function() {
-      it("should add a record to a given collection", function(done) {
-        adapter.addOne("test_coll", {a: 1, b: 2}, function(err, record) {
-          expect(err).to.be.a("null");
-          expect(record).to.be.a("object");
-          expect(record.a).eql(1);
-          done();
-        });
-      });
-    });
-
-    describe("#getOne", function() {
-      it("should retrieve a record out of a query object", function(done) {
-        adapter.addOne("test_coll", {a: 1, b: 2}, function(err) {
-          adapter.getOne("test_coll", {a: 1}, function(err, record) {
-            expect(err).to.be.a("null");
-            expect(record).to.be.a("object");
-            expect(record.a).eql(1);
+        it("should give an error on no record found", function(done) {
+          adapter.getOne("test_coll", {x: 42}, function(err, record) {
+            expect(err).to.be.a.instanceOf(Error);
+            expect(err.message).to.match(/No record found matching query/);
             done();
           });
         });
       });
 
-      it("should give an error on no record found", function(done) {
-        adapter.getOne("test_coll", {x: 42}, function(err, record) {
-          expect(err).to.be.a.instanceOf(Error);
-          expect(err.message).to.match(/No record found matching query/);
-          done();
-        });
-      });
-    });
-
-    describe("#drop", function() {
-      it("should drop the database", function(done) {
-        adapter.addOne("bar", {a: 1}, function() {
-          adapter.drop(function(err) {
-            expect(err).to.be.a("null");
-            adapter.getOne("test_coll", {a: 1}, function(err, record) {
-              expect(err).to.be.a("object");
-              done();
+      describe("#drop", function() {
+        it("should drop the database", function(done) {
+          adapter.addOne("bar", {a: 1}, function() {
+            adapter.drop(function(err) {
+              expect(err).to.be.a("null");
+              adapter.getOne("test_coll", {a: 1}, function(err, record) {
+                expect(err).to.be.a("object");
+                done();
+              });
             });
           });
         });
       });
+    });
+  }
+
+  describe("constructed", function() {
+    testAdapter("MongoAdapter", function createMongoAdapter() {
+      return new MongoAdapter('mongodb://127.0.0.1:27017/loop_test');
+    });
+
+    testAdapter("MemoryAdapter", function createMemoryAdapter() {
+      return new MemoryAdapter();
     });
   });
 });
