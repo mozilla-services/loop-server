2fb55cca15ed251d38ca771e0335e641ef0791f5
Rémy HUBSCHER <rhubscher@mozilla.com>
2014-04-10 11:57:30 -0700

    Add memory storage.

---
+++ loop/storage/memory.js
@@ -0,0 +1,117 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+
+function Storage(settings, options) {
+  var _db = {
+    urlsRevocationStore: [],
+    urlsStore: [],
+    callsStore: []
+  };
+
+  function find(name, query, cb) {
+    cb(null, _db[name].filter(function(record) {
+      return Object.keys(query).every(function(field) {
+        return record[field] === query[field];
+      });
+    })); 
+  }
+
+  function findOne(name, query, cb) {
+    find(name, query, function(err, records) {
+      if (records.length === 0) {
+        cb(null, null);
+        return;
+      }
+      cb(null, records.shift());
+    });
+  }
+
+  function deleteItem(name, query, cb) {
+    find(name, query, function(err, records) {
+      if (records.length === 0) {
+        cb(null, null);
+        return;
+      }
+      
+      var recordsCopy = JSON.parse(JSON.stringify(records));
+      records.forEach(function(item) {
+        var i = _db[name].indexOf(item);
+        if (i != -1) {
+          _db[name].splice(i, 1);
+        }
+      });
+      
+      cb(null, recordsCopy);
+    });
+  }
+
+  return {
+
+    get name() {
+      return _settings.engine;
+    },
+
+    revokeURLToken: function(token, callback) {
+      var ttl = (token.expires * 60 * 60 * 1000) - new Date().getTime();
+      var record = {
+        uuid: token.uuid,
+        ttl: ttl
+      };
+      deleteItem('urlsRevocationStore', {uuid: token.uuid}, function(){
+        _db.urlsRevocationStore.push(record);
+        callback(null, record);
+      });
+    },
+  
+    isURLRevoked: function(urlId, callback) {
+      findOne('urlsRevocationStore', {uuid: urlId}, callback);
+    },
+  
+    addUserSimplePushURL: function(userMac, simplepushURL, callback) {
+      deleteItem('urlsStore', {userMac: userMac}, function(){
+        var record = {
+          userMac: userMac,
+          simplepushURL: simplepushURL
+        };
+        _db.urlsStore.push(record);
+        callback(null, record);
+      });
+    },
+  
+    getUserSimplePushURLs: function(userMac, callback) {
+      find('urlsStore', {userMac: userMac}, callback);
+    },
+  
+    addUserCall: function(userMac, call, callback) {
+      _db.callsStore.push(call);
+      callback(null, call);
+    },
+  
+    getUserCalls: function(userMac, callback) {
+      find('callsStore', {userMac: userMac}, callback);
+    },
+  
+    getCall: function(callId, callback) {
+      findOne('callsStore', {callId: callId}, callback);
+    },
+  
+    deleteCall: function(callId, callback) {
+      deleteItem('callsStore', {callId: callId}, callback);
+    },
+  
+    drop: function(callback) {
+      _db = {
+        urlsRevocationStore: [],
+        urlsStore: [],
+        callsStore: []
+      };
+      callback();
+    }
+  };
+}
+
+module.exports = Storage;
