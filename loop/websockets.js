c15e8926537042e8f6c966f7e5ed58efa0e0c176
Alexis Métaireau <alexis@notmyidea.org>
2014-06-18 20:04:12 +0200

    Added comments, finished the state machine implementation

--- loop/websockets.js
+++ loop/websockets.js
@@ -13,6 +13,7 @@ function MessageHandler(pubsub, storage) {
 }
 
 MessageHandler.prototype = {
+
   /**
    * Parses a message and dispatches it to the right handler.
    **/
@@ -24,6 +25,8 @@ MessageHandler.prototype = {
       action: "handleAction"
     };
 
+    var messageType = inboundMessage.messageType;
+
     if (!handlers.hasOwnProperty(messageType)) {
       cb(new Error("Unknown messageType"));
       return;
@@ -34,6 +37,15 @@ MessageHandler.prototype = {
     });
   },
 
+  /**
+   * Handles the hello message.
+   *
+   * Does authentication (checks that the passed hawk credentials are valid,
+   * and answers with the status of the call.
+   *
+   * In addition to that, listens on the pubsub for forward events about this
+   * call.
+   **/
   handleHello: function(session, message, cb) {
     // Check that message contains requireParams, otherwise return an error.
     try {
@@ -79,7 +91,7 @@ MessageHandler.prototype = {
             state: state
           });
         });
-        
+
         // Alert clients on call state changes.
         this.pubsub.on("message", function(channel, receivedState) {
           if (channel !== session.callId) {
@@ -93,6 +105,13 @@ MessageHandler.prototype = {
     });
   },
 
+  /**
+   * Handles state changes submitted by the clients.
+   *
+   * Update the current state of the call using the information passed by the
+   * clients. Once the new state is defined, broadcast it to the interested
+   * parties using the pubsub.
+   **/
   handleAction: function(session, message, cb) {
     // We received a session changed
     try {
@@ -121,48 +140,67 @@ MessageHandler.prototype = {
 
     // Get current state
     this.storage.getCallState(session.callId, function(err, currentState) {
-      var stateMachine = {
-        "accept": [
-          ["alerting", "connecting"]
-        ],
-        "media-up": [
-          ["connecting", "half-connected"],
-          ["half-connected", "connected"],
-          validateState
-        ]
+
+      // Ensure half-connected is not send twice by the same party.
+      var validateState = function(callId, currentState, event) {
+        return true;
       };
 
-      // Get the validator
-      var validator = stateMachine[event].pop();
-      if (typeof validator === "object") {
-        stateMachine[event].push(validator);
-        validator = undefined;
-      }
+      var stateMachine = {
+        "accept": {
+          transitions: [
+            ["alerting", "connecting"]
+          ],
+        },
+        "media-up": {
+          transitions: [
+            ["connecting", "half-connected"],
+            ["half-connected", "connected"]
+          ],
+          validator: validateState
+        }
+      };
 
       var handled = false;
 
-      stateMachine[event].forEach(function(transition, key) {
-        if (transition[0] === currentState) {
-          handled = true;
-          if (validator !== undefined) {
-            if (validator(callId, currentState, event)) {
-              this.broadcastState(session.callId, stateMachine[event][1], cb);
+      if (stateMachine.hasOwnProperty(event)) {
+        var validated = true;
+        var state = stateMachine[event];
+
+        state.transitions.forEach(function(transition, key) {
+          if(transition[0] === currentState) {
+            handled = true;
+            var validator = stateMachine[event].validator;
+            if (validator !== undefined) {
+              if (!validator(session.callId, currentState, event)) {
+                validated = false;
+              }
+            }
+            if (validated === true) {
+
+              // In case we're connected, close the connection.
+              var terminate;
+              if (transition[1] === "connected") {
+                terminate = "closeConnection";
+              }
+              this.broadcastState(session.callId, transition[1], terminate, cb);
             }
             return;
           }
-          this.broadcastState(session.callId, stateMachine[event][1], cb);
-        }
-      });
-
+        });
+      }
       if (!handled) {
         cb(
-          new Error("No transition from " + currentState +
-                    " state with " + event + " event.")
+          new Error("No transition from " + currentState + " state with " +
+                    event + " event.")
         );
       }
     });
   },
 
+  /**
+   * Broadcast the call-state data to the interested parties.
+   **/
   broadcastState: function(callId, state, closeConnection, cb) {
     if (cb === undefined) {
       cb = closeConnection;
@@ -171,18 +209,21 @@ MessageHandler.prototype = {
 
     this.storage.setCallState(callId, state, function(err) {
       if (err) throw err;
-      
+
       this.pubsub.publish(callId, state, function(err) {
         if (err) throw err;
-        
+
         cb(null, {
           messageType: "progress",
           state: state
         }, closeConnection);
       });
-    });      
+    });
   },
-  
+
+  /**
+   * Creates an error message to be consumed by the client.
+   **/
   createError: function(errorMessage) {
     return this.encode({
       messageType: "error",
@@ -198,6 +239,11 @@ MessageHandler.prototype = {
     return JSON.parse(data);
   },
 
+  /**
+   * Checks that the given parameters are present in the message.
+   *
+   * Message is passed as the last argument of the function.
+   **/
   requireParams: function() {
     var params = Array.prototype.slice.call(arguments);
     var inboundMessage = params.shift();
@@ -215,6 +261,9 @@ MessageHandler.prototype = {
 };
 
 module.exports = function(storage, logError, conf) {
+  /**
+   * Allow a server to register itself as a websocket provider.
+   **/
   var register = function(server) {
     var pubsub = new PubSub(conf.get('pubsub'));
     var messageHandler = new MessageHandler(pubsub, storage);
@@ -225,24 +274,25 @@ module.exports = function(storage, logError, conf) {
       // Check authentication with hawk.
       ws.on('message', function(data) {
         try {
-          messageHandler.dispatch(session, data, function(err, outboundMessage, terminate) {
-            // Regular error
-            if (err) {
-              ws.send(messageHandler.createError(err.message));
-              ws.close();
-            }
+          messageHandler.dispatch(session, data,
+            function(err, outboundMessage, terminate) {
+              // Handle regular error.
+              if (err) {
+                ws.send(messageHandler.createError(err.message));
+                ws.close();
+              }
 
-            ws.send(outboundMessage);
-            if (terminate === "closeConnection") {
-              ws.close();
-            }
-          });
+              ws.send(outboundMessage);
+              if (terminate === "closeConnection") {
+                ws.close();
+              }
+            });
         } catch(e) {
-          // Programmation error.
+          // Handle programmation / uncatched errors.
           ws.send(messageHandler.createError(new Error("Service Unavailable")));
           ws.close();
         }
-      }); 
+      });
       ws.on('close', function() {
         ws.close();
       });
