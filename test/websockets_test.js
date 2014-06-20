bf5344286bc86647a7e0438ac24264116b74f6e5
Rémy HUBSCHER <rhubscher@mozilla.com>
2014-06-20 10:50:46 +0200

    Add test for broadcast.

--- test/websockets_test.js
+++ test/websockets_test.js
@@ -36,17 +36,18 @@ describe('websockets', function() {
 
     // Create the websocket client.
     client = new ws("ws://localhost:" + server.address().port);
-
-    // Generate Hawk credentials.
-    var token = new Token();
-    token.getCredentials(function(tokenId, authKey) {
-      hawkCredentials = {
-        id: tokenId,
-        key: authKey,
-        algorithm: "sha256"
-      };
-      userHmac = tokenId;
-      storage.setHawkSession(tokenId, authKey, done);
+    client.on('open', function() {
+      // Generate Hawk credentials.
+      var token = new Token();
+      token.getCredentials(function(tokenId, authKey) {
+        hawkCredentials = {
+          id: tokenId,
+          key: authKey,
+          algorithm: "sha256"
+        };
+        userHmac = tokenId;
+        storage.setHawkSession(tokenId, authKey, done);
+      });
     });
   });
 
@@ -56,50 +57,42 @@ describe('websockets', function() {
     client.close();
   });
 
-  it('should listen on the same port the app does', function(done) {
-    client.on('open', function() {
-      done();
-    });
-  });
+  it('should listen on the same port the app does');
 
   it('should reject bad authentication tokens', function(done) {
-    client.on('open', function() {
-      var callId = crypto.randomBytes(16).toString('hex');
-      createCall(callId, hawkCredentials.id, function(err) {
-        if (err) throw err;
-        client.on('message', function(data) {
-          var error = JSON.parse(data);
-          expect(error.messageType).eql('error');
-          expect(error.reason).eql('bad authentication');
-          done();
-        });
-        client.send(JSON.stringify({
-          messageType: 'hello',
-          authType: 'Hawk',
-          auth: '1234',
-          callId: callId
-        }));
+    var callId = crypto.randomBytes(16).toString('hex');
+    createCall(callId, hawkCredentials.id, function(err) {
+      if (err) throw err;
+      client.on('message', function(data) {
+        var error = JSON.parse(data);
+        expect(error.messageType).eql('error');
+        expect(error.reason).eql('bad authentication');
+        done();
       });
+      client.send(JSON.stringify({
+        messageType: 'hello',
+        authType: 'Hawk',
+        auth: '1234',
+        callId: callId
+      }));
     });
   });
 
   it('should reject an invalid callId with a valid hawk session',
     function(done) {
-      client.on('open', function() {
-        client.on('message', function(data) {
-          var error = JSON.parse(data);
-          expect(error.messageType).eql('error');
-          expect(error.reason).eql('bad callId');
-          done();
-        });
-
-        client.send(JSON.stringify({
-          messageType: 'hello',
-          authType: 'Hawk',
-          auth: hawkCredentials.id,
-          callId: '1234'
-        }));
+      client.on('message', function(data) {
+        var error = JSON.parse(data);
+        expect(error.messageType).eql('error');
+        expect(error.reason).eql('bad callId');
+        done();
       });
+
+      client.send(JSON.stringify({
+        messageType: 'hello',
+        authType: 'Hawk',
+        auth: hawkCredentials.id,
+        callId: '1234'
+      }));
     });
 
   it('should accept callers authenticating with the token url', function(done) {
@@ -114,70 +107,134 @@ describe('websockets', function() {
     });
     var callId = crypto.randomBytes(16).toString('hex');
 
-    client.on('open', function() {
-      createCall(callId, hawkCredentials.id, function(err) {
+    createCall(callId, hawkCredentials.id, function(err) {
+      if (err) throw err;
+      storage.setCallState(callId, "init", function(err) {
         if (err) throw err;
-        storage.setCallState(callId, "init", function(err) {
-          if (err) throw err;
-          client.on('message', function(data) {
-            var message = JSON.parse(data);
-            expect(message.messageType).eql("hello");
-            expect(message.state).eql("init");
-            done();
-          });
-
-          client.send(JSON.stringify({
-            messageType: 'hello',
-            authType: "Token",
-            auth: tokenWrapper.token,
-            callId: callId
-          }));
+        client.on('message', function(data) {
+          var message = JSON.parse(data);
+          expect(message.messageType).eql("hello");
+          expect(message.state).eql("init");
+          done();
         });
+
+        client.send(JSON.stringify({
+          messageType: 'hello',
+          authType: "Token",
+          auth: tokenWrapper.token,
+          callId: callId
+        }));
       });
     });
   });
 
   it('should return the state of the call', function(done) {
-    var currentTimestamp = Date.now();
     var callId = crypto.randomBytes(16).toString('hex');
 
     var messageCounter = 0;
 
-    client.on('open', function() {
-      storage.addUserCall(hawkCredentials.id, {
-        'callerId': 'Remy',
-        'callId': callId,
-        'userMac': hawkCredentials.id,
-        'sessionId': '1234',
-        'calleeToken': '1234',
-        'timestamp': currentTimestamp
-      }, function(err) {
+    storage.addUserCall(hawkCredentials.id, {
+      'callerId': 'Remy',
+      'callId': callId,
+      'userMac': hawkCredentials.id,
+      'sessionId': '1234',
+      'calleeToken': '1234',
+      'timestamp': Date.now()
+    }, function(err) {
+      if (err) throw err;
+      storage.setCallState(callId, "init", function(err) {
         if (err) throw err;
-        storage.setCallState(callId, "init", function(err) {
-          if (err) throw err;
-          client.on('message', function(data) {
-            var message = JSON.parse(data);
-            if (messageCounter === 0) {
-              expect(message.messageType).eql("hello");
-              expect(message.state).eql("init");
-              messageCounter++;
-            } else {
-              expect(message.messageType).eql("progress");
-              expect(message.state).eql("alerting");
-              done();
-            }
-          });
-
-          client.send(JSON.stringify({
-            messageType: 'hello',
-            authType: "Hawk",
-            auth: hawkCredentials.id,
-            callId: callId
-          }));
+        client.on('message', function(data) {
+          var message = JSON.parse(data);
+          if (messageCounter === 0) {
+            expect(message.messageType).eql("hello");
+            expect(message.state).eql("init");
+            messageCounter++;
+          } else {
+            expect(message.messageType).eql("progress");
+            expect(message.state).eql("alerting");
+            done();
+          }
         });
+
+        client.send(JSON.stringify({
+          messageType: 'hello',
+          authType: "Hawk",
+          auth: hawkCredentials.id,
+          callId: callId
+        }));
       });
     });
   });
 
-  it('should broadcast call state to other interested parties', function() {});
+  describe("with two clients", function() {
+    var client2;
+
+    beforeEach(function(done) {
+      // Create the websocket second client.
+      client2 = new ws("ws://localhost:" + server.address().port);
+      client2.on('open', function() { done(); });
+    });
+  
+    afterEach(function(done) {
+      client2.on('close', function() { done(); });
+      client2.close();
+    });
+
+    it('should broadcast call state to other interested parties',
+      function(done) {
+        var tokenManager = new tokenlib.TokenManager({
+          macSecret: conf.get('macSecret'),
+          encryptionSecret: conf.get('encryptionSecret')
+        });
+        var tokenWrapper = tokenManager.encode({
+          uuid: '1234',
+          user: hawkCredentials.id,
+          callerId: 'Alexis'
+        });
+        var callId = crypto.randomBytes(16).toString('hex');
+        var messageCounter = 0;
+        
+        storage.addUserCall(hawkCredentials.id, {
+          'callerId': 'Remy',
+          'callId': callId,
+          'userMac': hawkCredentials.id,
+          'sessionId': '1234',
+          'calleeToken': '1234',
+          'timestamp': Date.now()
+        }, function(err) {
+          if (err) throw err;
+          storage.setCallState(callId, "init", function(err) {
+            if (err) throw err;
+        
+            client2.on('message', function(data) {
+              var message = JSON.parse(data);
+              if (messageCounter === 0) {
+                expect(message.messageType).eql("hello");
+                expect(message.state).eql("init");
+                messageCounter++;
+              } else {
+                expect(message.messageType).eql("progress");
+                expect(message.state).eql("alerting");
+                done();
+              }
+            });
+        
+            client2.send(JSON.stringify({
+              messageType: 'hello',
+              authType: "Token",
+              auth: tokenWrapper.token,
+              callId: callId
+            }));
+        
+            client.send(JSON.stringify({
+              messageType: 'hello',
+              authType: "Hawk",
+              auth: hawkCredentials.id,
+              callId: callId
+            }));
+          });
+        });
+      });
+  });
 });
