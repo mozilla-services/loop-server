2d8d6fda5d0c9cfa6c21d20fd0a0fc457a3ce0d1
Peter deHaan <peter@deseloper.com>
2014-08-26 11:49:12 -0700

    updating Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,14 +5,13 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     "APP": {
-      "CODE_DIRS": "{,config/**/,loop/**/,test/**/}",
-      "COPYRIGHT": "This Source Code Form is subject to the terms of the Mozilla Public"
+      "CODE_DIRS": "{,config/**/,loop/**/,test/**/}"
     },
     "pkg": require("./package.json"),
 
     "copyright": {
       "options": {
-        "pattern": "<%= APP.COPYRIGHT %>"
+        "pattern": "This Source Code Form is subject to the terms of the Mozilla Public"
       },
       "src": "<%= eslint.src %>"
     },
@@ -26,13 +25,49 @@ module.exports = function (grunt) {
     },
 
     "shell": {
+      "outdated": {
+        "command": "npm outdated --depth 0"
+      },
       "shrinkwrap": {
         "command": "npm shrinkwrap --dev"
+      },
+      "rm-shrinkwrap": {
+        "command": "rm npm-shrinkwrap.json"
       }
     },
 
     "todo": {
-      "src": "<%= eslint.src %>"
+      "options": {
+        "marks": [
+          {
+            "name": 'FIX',
+            "pattern": /FIXME/,
+            "color": 'red'
+          },
+          {
+            "name": 'TODO',
+            "pattern": /TODO/,
+            "color": 'yellow'
+          },
+          {
+            "name": 'NOTE',
+            "pattern": /NOTE/,
+            "color": 'blue'
+          }, {
+            "name": 'XXX',
+            "pattern": /XXX/,
+            "color": 'yellow'
+          }, {
+            "name": 'HACK',
+            "pattern": /HACK/,
+            "color": 'red'
+          }
+        ]
+      },
+      "src": [
+        "<%= eslint.src %>",
+        "!Gruntfile.js"
+      ]
     },
 
     "validate-shrinkwrap": {
@@ -40,6 +75,7 @@ module.exports = function (grunt) {
   });
 
   grunt.registerTask("lint", ["eslint", "jsonlint"]);
-  grunt.registerTask("audit-shrinkwrap", ["shell:shrinkwrap", "validate-shrinkwrap"]);
+  grunt.registerTask("do-shrinkwrap", ["shell:shrinkwrap", "validate-shrinkwrap", "shell:rm-shrinkwrap"]);
+  grunt.registerTask("audit-shrinkwrap", ["do-shrinkwrap", "shell:outdated"]);
   grunt.registerTask("default", ["lint", "copyright"]);
 };
