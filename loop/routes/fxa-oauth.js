03d3d8d10541e8e7416270d58b9abdc2d1176d86
Alexis Métaireau <alexis@notmyidea.org>
2014-09-24 19:38:55 +0200

    Bug 1071247 — Fix POST fxa-oauth/token

--- loop/routes/fxa-oauth.js
+++ loop/routes/fxa-oauth.js
@@ -69,72 +69,74 @@ module.exports = function (app, conf, logError, storage, auth, validators) {
       storage.getHawkOAuthState(req.hawkIdHmac, function(err, storedState) {
         if (res.serverError(err)) return;
 
-        storage.clearHawkOAuthState(req.hawkIdHmac, function(err) {
+        var newState = randomBytes(32).toString('hex');
+        storage.setHawkOAuthState(req.hawkIdHmac, newState, function(err) {
           if (res.serverError(err)) return;
-        });
-
-        if (storedState !== state) {
-          // Reset session state after an attempt was made to compare it.
-          sendError(res, 400,
-            errors.INVALID_OAUTH_STATE, "Invalid OAuth state");
-          return;
-        }
-
-        // Trade the OAuth code for a token.
-        request.post({
-          uri: oauthConf.oauth_uri + '/token',
-          json: {
-            code: code,
-            client_id: oauthConf.client_id,
-            client_secret: oauthConf.client_secret
+          if (storedState !== state) {
+            // Reset session state after an attempt was made to compare it.
+            sendError(res, 400,
+              errors.INVALID_OAUTH_STATE, "Invalid OAuth state");
+            return;
           }
-        }, function (err, r, body) {
-          if (res.serverError(err)) return;
 
-          var token = body.access_token;
-          var tokenType = body.token_type;
-          var scope = body.scope;
-
-          // store the bearer token
-          storage.setHawkOAuthToken(req.hawkIdHmac, token);
-
-          // Make a request to the FxA server to have information about the
-          // profile.
-          request.get({
-            uri: oauthConf.profile_uri + '/profile',
-            headers: {
-              Authorization: 'Bearer ' + token
+          // Trade the OAuth code for a token.
+          request.post({
+            uri: oauthConf.oauth_uri + '/token',
+            json: {
+              code: code,
+              client_id: oauthConf.client_id,
+              client_secret: oauthConf.client_secret
             }
           }, function (err, r, body) {
             if (res.serverError(err)) return;
-            var data;
-            try {
-              data = JSON.parse(body);
-            } catch (e) {
-              sendError(res, 503, errors.BADJSON,
-                        e + " JSON: " + body);
-              return;
-            }
 
-            if (!data.hasOwnProperty("email")) {
-              sendError(res, 503, errors.BACKEND,
-                        "email not found in the oauth server request. " + data
-              );
-              return;
-            }
-            // Store the appropriate profile information into the database,
-            // associated with the hawk session.
-            var userHmac = hmac(data.email, conf.get('userMacSecret'));
-            storage.setHawkUser(userHmac, req.hawkIdHmac, function(err) {
+            var token = body.access_token;
+            var tokenType = body.token_type;
+            var scope = body.scope;
+
+            // store the bearer token
+            storage.setHawkOAuthToken(req.hawkIdHmac, token);
+
+            // Make a request to the FxA server to have information about the
+            // profile.
+            request.get({
+              uri: oauthConf.profile_uri + '/profile',
+              headers: {
+                Authorization: 'Bearer ' + token
+              }
+            }, function (err, r, body) {
               if (res.serverError(err)) return;
-              res.status(200).json({
-                token_type: tokenType,
-                access_token: token,
-                scope: scope
+              var data;
+              try {
+                data = JSON.parse(body);
+              } catch (e) {
+                sendError(res, 503, errors.BADJSON,
+                          e + " JSON: " + body);
+                return;
+              }
+
+              if (!data.hasOwnProperty("email")) {
+                sendError(res, 503, errors.BACKEND,
+                          "email not found in the oauth server request. " + data
+                );
+                return;
+              }
+              // Store the appropriate profile information into the database,
+              // associated with the hawk session.
+              var userHmac = hmac(data.email, conf.get('userMacSecret'));
+              storage.setHawkUser(userHmac, req.hawkIdHmac, function(err) {
+                if (res.serverError(err)) return;
+                res.status(200).json({
+                  token_type: tokenType,
+                  access_token: token,
+                  scope: scope
+                });
               });
             });
           });
+          
         });
+
       });
     });
 };
