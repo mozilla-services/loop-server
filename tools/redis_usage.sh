3602072ef100ebb68ac682f06948b0b0eaba540c
Rémy HUBSCHER <rhubscher@mozilla.com>
2014-10-07 15:04:02 +0200

    Improve redis_usage.sh script.

--- tools/redis_usage.sh
+++ tools/redis_usage.sh
@@ -5,6 +5,12 @@ redis-cli -h "$host" info
 echo -e "========\n\n"
 for key in "spurl" "callurl" "userUrls" "call" "userCalls" "callstate" "hawkuser" "userid" "hawk" "oauth.token" "oauth.state"
 do
-	echo -n "Number of keys for $key "
-	redis-cli -h $host KEYS "$key.*" | wc -l | sort -k 2 -nr
+	echo "Keys for $key.*"
+	echo "====================="
+	redis-cli -h $host KEYS "$key.*" | grep -v '^$' > tmp
+	head -n 5 tmp
+	echo -n "Total of keys: "
+	cat tmp | wc -l | sort -k 2 -nr
+	rm tmp
+	echo -e "\n"
 done
