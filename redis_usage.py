d15c67352df7a2320ef2bdf6d33609bff4cccc11
Rémy HUBSCHER <rhubscher@mozilla.com>
2015-01-06 10:02:07 +0100

    Bug 1067401 — Redis Usage estimate script.

--- redis_usage.py
+++ redis_usage.py
@@ -3,9 +3,11 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import json
 
 BARE_REDIS = 600000
-USER_WEIGHT = 280
-CALL_WEIGHT = 1365
+USER_WEIGHT = 300
+CALL_WEIGHT = 1400
 REVOCATION_WEIGHT = 150
+ROOM_WEIGHT = 700
+ROOM_PARTICIPANT_WEIGHT = 550
 
 
 def get_version():
@@ -15,11 +17,14 @@ def get_version():
         return package['version']
 
 
-def compute_redis_usage(users, daily_calls, monthly_revocations):
+def compute_redis_usage(users, daily_calls, monthly_revocations, rooms, rooms_participants):
     """Computes the redis usage, in megabytes"""
     return ((users * USER_WEIGHT +
-            daily_calls * users * CALL_WEIGHT +
-            monthly_revocations * REVOCATION_WEIGHT + BARE_REDIS)
+             daily_calls * users * CALL_WEIGHT +
+             monthly_revocations * REVOCATION_WEIGHT +
+             rooms * users * ROOM_WEIGHT +
+             rooms * users * rooms_participants * ROOM_PARTICIPANT_WEIGHT +
+             BARE_REDIS)
             / 1024) / 1024
 
 
@@ -29,41 +34,70 @@ def main():
         formatter_class=ArgumentDefaultsHelpFormatter)
 
     parser.add_argument(
+        '-u', '--users',
         dest='users',
         help='The number of users that will be using this server',
-        type=int)
+        type=int,
+        default=1000)
 
     parser.add_argument(
+        '-c', '--calls',
         dest='daily_calls',
+        nargs='?',
         help='The number calls that will be done per user (average)',
-        type=int)
+        type=int,
+        default=1)
 
     parser.add_argument(
+        '-m', '--revocations',
         dest='monthly_revocations',
         nargs='?',
         help='The number of revocation, per month',
         type=int,
         default=0)
 
+    parser.add_argument(
+        '-r', '--rooms',
+        dest='rooms',
+        nargs='?',
+        help='The number of rooms per user',
+        type=int,
+        default=5)
+
+    parser.add_argument(
+        '-p', '--participants',
+        dest='rooms_participants',
+        nargs='?',
+        help='The average number of rooms participants',
+        type=float,
+        default=1)
+
     args = parser.parse_args()
 
     usage = compute_redis_usage(
         args.users,
         args.daily_calls,
-        args.monthly_revocations
+        args.monthly_revocations,
+        args.rooms,
+        args.rooms_participants
     )
 
-    text = ("loop-server: v{version}\n"
-            "Usage for {users} users, with {daily_calls} daily calls "
-            "per user and {monthly_revocations} monthly revocations is "
-            "{usage} MBytes")
+    text = ("""loop-server: v{version}
+
+            The memory usage is {usage} MB for:
+             - {users} users
+             - with {daily_calls} daily calls per user,
+             - {monthly_revocations} call-urls monthly revocations,
+             - {rooms} rooms with around {rooms_participants} participants in it at all times\n""")
 
     print(text.format(**{
         'version': get_version(),
         'users': args.users,
         'daily_calls': args.daily_calls,
         'monthly_revocations': args.monthly_revocations,
-        'usage': usage
+        'rooms': args.rooms,
+        'rooms_participants': args.rooms_participants,
+        'usage': '%.2f' % usage
     }))
 
 if __name__ == '__main__':
